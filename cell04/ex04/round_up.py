ff46aa28c3e5d354d6e27be86cfc67bb3c42966d
Thanyasorn Sawanglerk <kaopan1971@gmail.com>
2025-09-16 23:59:17 +0700

    Delete cell04/ex04 directory

--- cell04/ex04/round_up.py
+++
@@ -1,8 +0,0 @@
-user_input = input("Give me a number: ")
-number = float(user_input)
-rounded_down_number = round(number)
-
-if number > rounded_down_number:
-    print(rounded_down_number + 1)
-else:
-    print(int(number))
